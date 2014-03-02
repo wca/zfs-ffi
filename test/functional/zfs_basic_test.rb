9458abed7124675589134aec7f8090966b977dd9
Will Andrews <will@firepipe.net>
2014-03-02 15:35:36 -0700

    Test whitespace embellishment.

--- test/functional/zfs_basic_test.rb
+++ test/functional/zfs_basic_test.rb
@@ -18,10 +18,12 @@ class TestBasic < Test::Unit::TestCase
       hsh
     end
   end
+
   def run_cmd(cmd)
     puts "Running: #{cmd}" if ENV["DEBUG"]
     system cmd or raise "Command failed!"
   end
+
   def setup
     avail_disk = geom_disks.find do |name, h|
       !(name =~ /da\d+$/).nil? && (h[:rd] + h[:wr] + h[:ex]).zero?
@@ -31,9 +33,11 @@ class TestBasic < Test::Unit::TestCase
     system "zpool destroy -f #{@poolname} 2>/dev/null"
     run_cmd "zpool create -f #{@poolname} #{avail_disk[0]}"
   end
+
   def teardown
     run_cmd "zpool destroy -f #{@poolname}"
   end
+
   def test_zfs_pool_lookup_works
     pool = nil
     ZFS::Pool.each {|p| pool = p if p.name == @poolname}
