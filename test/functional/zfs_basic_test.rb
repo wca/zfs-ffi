2c0aa2958bde3f5a222341772fea5d134ba1148f
Will Andrews <will@firepipe.net>
2014-03-02 21:19:59 -0700

    Refactor common zfs test routines to test_helper.
    Improve the pool setup & teardown:
    - Skip any pool tests if not running as root.
    - If no geom disk is available, try using a memory disk.
    - If pool setup fails during the critical stage, always call teardown.
    - Randomize the pool name to avoid simultaneous test conflicts.
    
    Refactor TestBasic#test_zfs_pool_lookup_works to use ZFS::Pool.find_by_name,
    and assert_not_nil().

--- test/functional/zfs_basic_test.rb
+++ test/functional/zfs_basic_test.rb
@@ -1,47 +1,17 @@
 require 'test_helper'
 
 class TestBasic < Test::Unit::TestCase
-  def geom_disks
-    diskname = nil
-    lines = `geom disk list 2>/dev/null`.split("\n") rescue []
-    lines.inject({}) do |hsh, line|
-      if line =~ /^Geom name: ([a-z]+\d+)/
-        diskname = $1
-      elsif line =~ /^   Mode: r(\d)w(\d)e(\d)/
-        raise "Error parsing geom: found mode without a disk" unless diskname
-        hsh[diskname] = {
-          :name => diskname,
-          :rd => $1.to_i, :wr => $2.to_i, :ex => $3.to_i
-        }
-        diskname = nil
-      end
-      hsh
-    end
-  end
-
-  def run_cmd(cmd)
-    puts "Running: #{cmd}" if ENV["DEBUG"]
-    system cmd or raise "Command failed!"
-  end
+  include ZFSTest
 
   def setup
-    avail_disk = geom_disks.find do |name, h|
-      !(name =~ /da\d+$/).nil? && (h[:rd] + h[:wr] + h[:ex]).zero?
-    end
-    raise "Can't find an available disk to use" unless avail_disk
-    @poolname = "#{self.class.name}"
-    system "zpool destroy -f #{@poolname} 2>/dev/null"
-    run_cmd "zpool create -f #{@poolname} #{avail_disk[0]}"
+    pool_setup
   end
 
   def teardown
-    run_cmd "zpool destroy -f #{@poolname}"
+    pool_teardown
   end
 
   def test_zfs_pool_lookup_works
-    pool = nil
-    ZFS::Pool.each {|p| pool = p if p.name == @poolname}
-    puts "pool=#{pool.inspect}"
-    assert(!pool.nil?)
+    assert_not_nil(ZFS::Pool.find_by_name(@poolname))
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 require 'test/unit'
 # required for class/module-level stubs.
 require 'mocha/setup'
@@ -9,5 +10,66 @@ class Object
     class << self
       self
     end
+  end
+end
+
+# Common routines for ZFS tests.  Include this module in every test class to
+# get access to these as instance methods.
+module ZFSTest
+  def geom_disks
+    diskname = nil
+    (`geom disk list 2>/dev/null`.split("\n") rescue []).inject({}) do |hsh, l|
+      if l =~ /^Geom name: ([a-z]+\d+)/
+        diskname = $1
+      elsif l =~ /^   Mode: r(\d)w(\d)e(\d)/
+        raise "Error parsing geom: found mode without a disk" if diskname.nil?
+        hsh[diskname] = {
+          :name => diskname,
+          :rd => $1.to_i, :wr => $2.to_i, :ex => $3.to_i
+        }
+        diskname = nil
+      end
+      hsh
+    end
+  end
+
+  def available_disks
+    geom_disks.select do |name, h|
+      !name.match(/da\d+$/).nil? && (h[:rd] + h[:wr] + h[:ex]).zero?
+    end
+  end
+
+  def run_cmd(cmd)
+    $stderr.puts "Running: #{cmd}" if ENV["DEBUG"]
+    system(cmd)
+    raise "Command failed!" unless $?.success?
+  end
+
+  def pool_setup
+    skip("Must run ZFS pool tests as root!") unless Process.uid.zero?
+
+    avail_disk = available_disks.first
+    if avail_disk.nil?
+      $stderr.puts "No geom disks available, trying memory disk..."
+      avail_disk = `mdconfig -a -t malloc -s 1g 2>/dev/null`
+      if avail_disk.empty?
+        raise "Unable to autodetect a disk for the temporary pool"
+      end
+      @memdisk = avail_disk
+    end
+
+    begin
+      @poolname = "#{self.class.name}_#{SecureRandom.urlsafe_base64(12)}"
+      run_cmd("zpool create #{@poolname} #{avail_disk}")
+      yield if block_given?
+    rescue StandardError => e
+      pool_teardown
+      raise
+    end
+  end
+
+  def pool_teardown
+    run_cmd("zpool destroy -f #{@poolname}")
+    run_cmd("mdconfig -d -u #{@memdisk}") if @memdisk
   end
 end
